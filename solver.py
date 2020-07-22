d85176ba4ccd217169cd8f687da751288a6aa2b3
NickH2017 <NicolasHernandezOne@gmail.com>
2020-07-21 17:46:30 -0700

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,24 @@
+#
+# Python Solvable Sudoku Game
+#
+
+# Prints the sudoku board
+def printBoard(arr):
+        
+    for row in range(9):
+        for column in range(9):
+            print(arr[row][column])
+
+
+
+board = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]]
+
+printBoard(board)
